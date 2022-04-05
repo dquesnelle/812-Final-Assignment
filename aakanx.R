0e45b003a289763c8a0fed7e9939c74811515ffe
Aakanx <aakanx.panchal@queensu.ca>
2022-04-05 17:49:51 -0400

    trying stuff

--- aakanx.R
+++ aakanx.R
@@ -1,3 +1,8 @@
+# how to setup everything with GitHub:
+# clone GitHub repo from webpage, download GitHub Desktop
+# open in RStudio through GitHub Desktop
+# top right of RStudio, New Project, Existing Directory (everything you just cloned)
+
 print("test")
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -8,29 +13,53 @@ library(msa)
 
 system.file("tex", "texshade.sty", package="msa")
 
-mySequences <- readDNAStringSet("test_sequences.fasta")
-mySequences
+seqs <- readDNAStringSet("test_sequences.fasta")
+seqs
 
-myFirstAlignment <- msa(mySequences)
-print(myFirstAlignment, show="complete")
+aligned <- msa(seqs)
+print(aligned, show = "complete")
 
-msaPrettyPrint(myFirstAlignment, output="tex")
+msaPrettyPrint(aligned, 
+               output = "pdf", 
+               showNames = "none", 
+               showLogo = "none", 
+               showConsensus = "none", 
+               showLegend = FALSE, 
+               askForOverwrite = FALSE) # error, but .tex is usable
 
-###
+### pdf
+
+# after running the previous lines
+# this is what's working on Windows and makes a nicer .pdf
+# download MiKTeX and TeXstudio (together as proTeXt https://ctan.org/pkg/protext)
+# open myFirstAlignment.tex in TeXstudio and press the button "Build & View" or hit F5
+# whatever files were involved in this process are in ./files right now
+# everything doesn't have to be in R: we can try tex to pdf in shell, calling MiKTeX to make it happen
+
+# trying again
+# needed to add following line in Sys.getenv("PATH") using Sys.setenv(PATH="...;...;...;___")
+# C:\\Users\\aakan\\AppData\\Local\\Programs\\MiKTeX 2.9\\miktex\\bin\\x64
+# run following two lines:
+# oldPath <- Sys.getenv("PATH")
+# Sys.setenv(PATH = paste(oldPath, "C:\\Users\\aakan\\AppData\\Local\\Programs\\MiKTeX 2.9\\miktex\\bin\\x64", sep = ";")); Sys.getenv("PATH")
+# problem with pdflatex.exe
+
+### other stuff
 
 install.packages("seqinr")
 
 hemoSeq <- readDNAStringSet("test_sequences.fasta")
 hemoAln <- msa(hemoSeq)
 
-hemoAln2 <- msaConvert(hemoAln, type="seqinr::alignment")
+hemoAln2 <- msaConvert(hemoAln, type = "seqinr::alignment")
 
 
 library(seqinr)
 d <- dist.alignment(hemoAln2, "identity")
-as.matrix(d)[1:3, "HBA1_Homo_sapiens", drop=FALSE]
+as.matrix(d)[1:3, "HBA1_Homo_sapiens", drop = FALSE]
 
 library(ape)
 hemoTree <- nj(d)
-plot(hemoTree, main="Phylogenetic Tree of Hemoglobin Alpha Sequences")
+plot(hemoTree, main = "Phylogenetic Tree of Hemoglobin Alpha Sequences")
+
 
