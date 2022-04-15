7f936d531bf55b57ca633cb888b5399179166ed7
Dan Quesnelle <d.quesnelle@queensu.ca>
2022-04-14 21:58:39 -0400

    updated blast

--- DQ_Blast.R
+++ DQ_Blast.R
@@ -14,26 +14,17 @@ Seq_Blast = function(InputName, E, OutputName) {
   
   AA = readDNAStringSet(InputName) # Read test sequence
     
-  AAblast = blastSequences(paste(AA), expect = E, as = 'data.frame') # Blast sequence to find similar sequences
+  AAblast = blastSequences(paste(AA), 
+                           expect = E, 
+                           as = 'data.frame') # Blast sequence to find similar sequences
 
-  # Remove sequences so that only 1 per species is left
-  ##First = grep("loti", AAblast$Hit_def)
-  #Indivs = unique(AAblast$Hit_accession)
-  #Indivs
-  
-  #grep(Indivs, AAblast)
-  
   write.fasta(sequences = AAblast, 
-              file.out = OutputName) # Write the individual sequences to a new fasta file for use in the alignment
-
-  
+              file.out = OutputName) # Write the blast result to a new fasta file for use in the alignment
   
 }
 
-
-Seq_Blast("Mesorhizobium.fasta", 10, "testalign.fasta")
-
+#Seq_Blast("Mesorhizobium.fasta", 10, "testalign.fasta") # For testing purposes
 
 args = commandArgs(trailingOnly = TRUE)
-Seq_Blast(args[c(1, 2, 3)]) # here we define our FileName parameter to be used in the command line
+Seq_Blast(args[c(1, 2, 3)]) # here we define our parameters for the command line
 
