4628d4adc7ebc8f52aca7cec7feccff2a1cdab29
Dan Quesnelle <d.quesnelle@queensu.ca>
2022-04-18 18:17:22 -0400

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,13 @@
+# Run this script to run the overall project
 
-bash sequence_generator.sh
+# Retrieve nifh gene sequences
+bash sequence_generator.sh 
 
+# Run alignment on nifh sequences
 bash ./2-MSA/msa.sh
 
+# Filter out poorly aligned sequences
 Python ratio_test.py
 
+# Create distance matrix and phylogenetic tree
 Rscript distrance_matrix_and_tree.R
