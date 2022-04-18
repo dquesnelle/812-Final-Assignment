1b2afc94c4f1c7cd623565252a27002969b525d1
Aakanx <aakanx.panchal@queensu.ca>
2022-04-18 15:14:56 -0400

    ap

--- 2-MSA/msa.sh
+++ 2-MSA/msa.sh
@@ -1,11 +1,14 @@
 Rscript ./2-MSA/msa.R "nifh_sequences1.fasta" "out.fasta" # arguments are infile and outfile
 
+# for testing, use next line:
+# Rscript ./2-MSA/msa.R "./test-files/test_sequences.fasta" "./test-files/testout.fasta"
+
 # get Perl https://www.perl.org/get.html and make sure it is added to PATH
 # get MView https://desmid.github.io/mview/index.html#installation and make sure it is added to PATH
 # restart computer for PATH to be updated in R
 # check R PATH by writing in R console: Sys.getenv("PATH")
-# this part was done on Windows, which is why the batch file mview.bat is used
-mview.bat -in fasta -width 100 -html head -bold -css on -coloring identity -moltype dna ./out.fasta > figure1.html; open figure1.html
+# this part was done on Windows, which is why the batch file version of mview is used
+mview.bat -in fasta -width 100 -html head -bold -css on -coloring identity -moltype dna ./2-MSA/out.fasta > ./2-MSA/figure1.html
   # -in fasta: input format
   # -width 100: 100 bp per line shown
   # -html head: output to html file
@@ -14,7 +17,7 @@ mview.bat -in fasta -width 100 -html head -bold -css on -coloring identity -molt
   # -coloring identity: coloured with respect to identity/match with reference sequence
   # -moltype dna: colouring by pyrimidines, purines
   # first sequence in file is reference sequence by default
-mview.bat -in fasta -width 100 -html head -bold -css on -coloring mismatch -colormap red -ref 32 -sort pid ./out.fasta > figure2.html; open figure2.html
+mview.bat -in fasta -width 100 -html head -bold -css on -coloring mismatch -colormap red -ref 32 -sort pid ./2-MSA/out.fasta > ./2-MSA/figure2.html
   # -in fasta: input format
   # -width 100: 100 bp per line shown
   # -html head: output to html file
