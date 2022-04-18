d9d3d2c824fd75700cd2370b9d51dcb9b339fffd
Dan Quesnelle <d.quesnelle@queensu.ca>
2022-04-18 15:14:45 -0400

    Update sequence_generator.sh

--- sequence_generator.sh
+++ sequence_generator.sh
@@ -4,7 +4,7 @@ Rscript gene_IDs.r "nifh_accessions.csv"
 # Install EDirect
 sh -c "$(curl -fsSL ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh)"
 
-# Copy and paste gene IDs into loop in gene_id_parser.sh
+# Copy and paste gene IDs into loop in gene_id_parser.sh. This has already been done.
 
 # Retrieve gene sequences using gene IDs
 bash gene_id_parser.sh
