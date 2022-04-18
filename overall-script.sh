11ec031065ef055bef3970d8ed0fe2e12a697b57
Dan Quesnelle <d.quesnelle@queensu.ca>
2022-04-18 15:02:09 -0400

    Update overall-script.sh

--- overall-script.sh
+++ overall-script.sh
@@ -1,2 +1,8 @@
-bash sequence_generator.sh
+
+# Install EDirect
+sh -c "$(curl -fsSL ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh)"
+
+# Retrieve gene sequences using gene IDs
+bash gene_id_parser.sh
+
 bash ./2-MSA/msa.sh
