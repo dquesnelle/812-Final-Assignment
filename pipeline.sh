754feee18eb3a28d1074d0193d0ebf810b7e923d
Xinran Li <17xl12@queensu.ca>
2022-04-19 01:54:48 -0400

    update vars in pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,16 @@
 # Run this script to run the overall project
+nifh_fasta="./nifh_sequences1.fasta"
+fasta_ori="./2-MSA/out.fasta"
+dm_path="./DMinput.fasta"
 
 # Retrieve nifh gene sequences
 bash sequence_generator.sh 
 
 # Run alignment on nifh sequences
-bash ./2-MSA/msa.sh
+bash ./2-MSA/msa.sh $nifh_fasta $fasta_ori
 
 # Filter out poorly aligned sequences
-Python ratio_test.py
+Python ratio_test.py $fasta_ori $dm_path
 
 # Create distance matrix and phylogenetic tree
-Rscript distrance_matrix_and_tree.R
+Rscript distance_matrix_and_tree.R $dm_path
